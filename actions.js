0258b448a93cf38c8986c525b70e2bec3e72f6d6
Dmytro Momot <momotdev@gmail.com>
2021-11-11 23:57:41 +0200

    implemented add action func and recalculation chances method

--- actions.js
+++ actions.js
@@ -1,17 +1,22 @@
 let tempActions = [];
+let newActionInputValue = '';
+
 const actionsWrapper = document.getElementById('actions-list');
+const actionInput = document.getElementById('new-action');
+const addActionBtn = document.getElementById('add-action');
 
 function getActions() {
-	//chrome.storage.sync.clear();
 	chrome.storage.sync.get("actions", ({actions}) => {
 		if (actions) {
 			tempActions = actions;
 			renderActions(tempActions);
+			calculateActionsChance(tempActions);
 		} else {
 			chrome.storage.sync.set({
 				actions: [
 					{name: "Temp Action 1", chance: 10, amount: 10},
-					{name: "Temp Action 2", chance: 90, amount: 10},
+					{name: "Temp Action 2", chance: 10, amount: 10},
+					{name: "Temp Action 3", chance: 80, amount: 10},
 				]
 			});
 		}
@@ -24,6 +29,35 @@ actionsWrapper.addEventListener('click', (event) => {
 		deleteAction(target.dataset.name);
 	}
 })
+actionInput.addEventListener('change', (e) => newActionInputValue = e.target.value);
+addActionBtn.addEventListener('click', () => {
+	if (newActionInputValue) {
+		addAction(newActionInputValue);
+	}
+})
+
+function addAction(action) {
+	if (!action) return;
+
+	const actionObject = {
+		name: action,
+		chance: 0,
+		amount: 0
+	}
+
+	chrome.storage.sync.set({
+		actions: calculateActionsChance([...tempActions, actionObject])
+	}).then(() => getActions());
+}
+
+function calculateActionsChance(actions) {
+	const callsAmount = actions.reduce((acc, action) => acc + action.amount, 0);
+	return actions.map(action => ({...action, chance: getPercentFromNumber(action.amount, callsAmount)}));
+}
+
+function getPercentFromNumber(number, totalNumber) {
+	return (number / totalNumber) * 100;
+}
 
 function renderActions(actions) {
 	if (actions.length) {
@@ -52,3 +86,4 @@ function deleteAction(action) {
 }
 
 getActions();
+
