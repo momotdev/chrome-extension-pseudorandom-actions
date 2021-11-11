a2d7c1753adff0b4242d6d24f59d912df85e988e
Dmytro Momot <momotdev@gmail.com>
2021-11-12 01:23:11 +0200

    implemented get action function and fortune chances algorithm

--- actions.js
+++ actions.js
@@ -4,19 +4,20 @@ let newActionInputValue = '';
 const actionsWrapper = document.getElementById('actions-list');
 const actionInput = document.getElementById('new-action');
 const addActionBtn = document.getElementById('add-action');
+const getActionBtn = document.getElementById('get-action');
 
 function getActions() {
+	//chrome.storage.sync.clear();
 	chrome.storage.sync.get("actions", ({actions}) => {
 		if (actions) {
-			tempActions = actions;
+			tempActions = calculateActionsChance(actions);
 			renderActions(tempActions);
-			calculateActionsChance(tempActions);
 		} else {
 			chrome.storage.sync.set({
 				actions: [
 					{name: "Temp Action 1", chance: 10, amount: 10},
-					{name: "Temp Action 2", chance: 10, amount: 10},
-					{name: "Temp Action 3", chance: 80, amount: 10},
+					{name: "Temp Action 2", chance: 10, amount: 5},
+					{name: "Temp Action 3", chance: 80, amount: 34},
 				]
 			});
 		}
@@ -34,7 +35,11 @@ addActionBtn.addEventListener('click', () => {
 	if (newActionInputValue) {
 		addAction(newActionInputValue);
 	}
-})
+});
+getActionBtn.addEventListener('click', () => {
+	showRandomizedAction(getAction());
+	getActions();
+});
 
 function addAction(action) {
 	if (!action) return;
@@ -42,12 +47,42 @@ function addAction(action) {
 	const actionObject = {
 		name: action,
 		chance: 0,
-		amount: 0
+		amount: 1
 	}
 
+	newActionInputValue = '';
+	actionInput.value = '';
 	chrome.storage.sync.set({
 		actions: calculateActionsChance([...tempActions, actionObject])
 	}).then(() => getActions());
+}
+
+function getAction() {
+	const random = Math.floor(Math.random() * 100);
+	const randomizedAction = calculateFortuneChances(tempActions).find(action => action.chance >= random);
+	tempActions.map(action => {
+		if (action.name === randomizedAction.name) action.amount++;
+	})
+
+	chrome.storage.sync.set({
+		actions: tempActions
+	});
+
+	return randomizedAction;
+}
+
+function showRandomizedAction(action) {
+	console.log(action)
+	const resultField = document.getElementById('action-result');
+
+	if (action) {
+		if (resultField.classList.contains('action-result-hide')) {
+			resultField.classList.remove('action-result-hide');
+			resultField.innerHTML = action.name;
+		} else {
+			resultField.innerHTML = action.name;
+		}
+	}
 }
 
 function calculateActionsChance(actions) {
@@ -55,8 +90,19 @@ function calculateActionsChance(actions) {
 	return actions.map(action => ({...action, chance: getPercentFromNumber(action.amount, callsAmount)}));
 }
 
+function calculateFortuneChances(actions) {
+	const fortuneActions = JSON.parse(JSON.stringify(actions));
+	let acc = 0;
+
+	for (const action in fortuneActions) {
+		acc += fortuneActions[action].chance;
+		fortuneActions[action].chance = acc;
+	}
+	return fortuneActions;
+}
+
 function getPercentFromNumber(number, totalNumber) {
-	return (number / totalNumber) * 100;
+	return +((number / totalNumber) * 100).toFixed(1);
 }
 
 function renderActions(actions) {
@@ -81,8 +127,8 @@ function renderActions(actions) {
 
 function deleteAction(action) {
 	tempActions = tempActions.filter(act => act.name.split(' ').join('') !== action);
-	chrome.storage.sync.set({actions: tempActions});
-	renderActions(tempActions);
+	chrome.storage.sync.set({actions: calculateActionsChance(tempActions)});
+	renderActions(calculateActionsChance(tempActions));
 }
 
 getActions();
